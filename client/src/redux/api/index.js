094f55724ea17919a8bb09f4e99040efd140d3bf
avezqureshi <aveznqureshi14@gmail.com>
2024-04-16 01:29:37 +0530

    feat: api endpoint updated

--- client/src/redux/api/index.js
+++ client/src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8800/" });
+const API = axios.create({ baseURL: "https://wobot.onrender.com/" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
